232a6ef36c4fc2fb628815bb06cbffa9966c2cd0
WalkerRout <89273280+WalkerRout@users.noreply.github.com>
2022-12-04 23:04:34 -0800

    Added example for fat pointer vector

--- src/main.c
+++ src/main.c
@@ -2,29 +2,34 @@
 #include <time.h>
 
 #define MVLA_IMPLEMENTATION
-#include "../mvla.h"
+#include "mvla.h"
 #undef  MVLA_IMPLEMENTATION
+
+void print_int(void *i) {
+  printf(" %d ", *(int*) i);
+}
 
 int main(void) {
   srand(time(NULL));
-  
-  Mat a = mat(5, 10);
-  Mat b = mat(5, 10);
+    
+  Mat a = mat(2, 2);
+  Mat b = mat(2, 2);
 
   mat_fill_rand(&a);
   mat_fill_rand(&b);
 
   printf("a: \n");
-  mat_print(a);
+  mat_print(mat_mul(mat_add(a, b), a));
+
+  int *test = fvec(sizeof(int));
+  *(int*)fvec_push(&test) = 3;
+  *(int*)fvec_push(&test) = 4;
+  *(int*)fvec_push(&test) = 5;
+  *(int*)fvec_push(&test) = 6;
+  *(int*)fvec_push(&test) = 7;
+  *(int*)fvec_push(&test) = 8;
+  *(int*)fvec_push(&test) = 9;
+  fvec_print(test, print_int);
   
-  printf("b: \n");
-  mat_print(b);
-  
-  printf("a + b: \n");
-  mat_print(mat_add(a, b));
-  
-  printf("a.T: \n");
-  mat_print(mat_transpose(a));
-    
   return 0;
 }
