bbf2411b0f3d41c4486fb8a01650e61ee70d5e4a
WalkerRout <89273280+WalkerRout@users.noreply.github.com>
2022-12-05 12:17:33 -0800

    fvec_pop_back, fvec_clone, fvec_map

--- src/main.c
+++ src/main.c
@@ -5,8 +5,14 @@
 #include "mvla.h"
 #undef  MVLA_IMPLEMENTATION
 
+void map_int(void *i) {
+  int *ref = i;
+  *ref += 1;
+  *ref *= 2;
+}
+
 void print_int(void *i) {
-  printf(" %d ", *(int*) i);
+  printf("%d ", *(int*) i);
 }
 
 int main(void) {
@@ -25,11 +31,24 @@ int main(void) {
   *(int*)fvec_push(&test) = 3;
   *(int*)fvec_push(&test) = 4;
   *(int*)fvec_push(&test) = 5;
+  fvec_pop_back(&test);
   *(int*)fvec_push(&test) = 6;
   *(int*)fvec_push(&test) = 7;
+  fvec_pop_back(&test);
   *(int*)fvec_push(&test) = 8;
   *(int*)fvec_push(&test) = 9;
+  fvec_pop_back(&test);
+  fvec_pop_back(&test);
+  fvec_pop_back(&test);
+  
+  fvec_map(test, map_int);
   fvec_print(test, print_int);
+
+  printf("Vector length: %u\n", fvec_length(test));
+
+  mat_free(&a);
+  mat_free(&b);
+  fvec_free(&test);
   
   return 0;
 }
