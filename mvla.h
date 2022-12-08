c021fa619457571172f9156c68e298e24aa1f2d0
WalkerRout <89273280+WalkerRout@users.noreply.github.com>
2022-12-07 21:27:32 -0800

    Remove fat pointer vectors (use fvec.h instead)

--- mvla.h
+++ mvla.h
@@ -125,15 +125,6 @@ typedef struct {
   unsigned int length;
   float *data;
 } Vec;
-
-typedef struct _FVecData {
-  unsigned int element_size; // size of each element
-  unsigned int capacity;     // number of elements that can fit
-  unsigned int bytes_alloc;  // element_size * capacity
-  unsigned int length;       // element count in the vector
-  //void *extra_data;        // an optional pointer to any extra data to be carried with the vector
-  unsigned char buffer[];
-} FVecData;
 // -----------------------------------------
 
 // -----------------------------------------
@@ -370,18 +361,6 @@ MVLADEF void vec_fill_rand(Vec *a);
 MVLADEF void vec_free(Vec *a);
 MVLADEF void vec_print(const Vec a);
 MVLADEF void vec_print_length(const Vec a);
-
-MVLADEF void *fvec(unsigned int element_size);
-MVLADEF void *fvecci(unsigned int element_size, unsigned int initial_size);
-MVLADEF FVecData *fvec_get_data(void *vector);
-//MVLADEF int fvec_has_space(FVecData *v_data);
-//MVLADEF void fvec_expand(FVecData **v_data);
-MVLADEF void *fvec_push(void **vector);
-MVLADEF void fvec_pop_back(void **vector);
-MVLADEF void fvec_map(void *vector, void(*func)(void*));
-MVLADEF unsigned int fvec_length(void *vector);
-MVLADEF void fvec_free(void **vector);
-MVLADEF void fvec_print(void *vector, void(*print_func)(void*));
 // -----------------------------------------
 
 // -----------------------------------------
@@ -418,32 +397,6 @@ MVLADEF void mat_print_rows_cols(const Mat a);
 ** HEADER ONLY IMPLEMENTATION
 */
 #ifdef MVLA_IMPLEMENTATION
-
-/*
-** @brief:   Produce the nearest highest power of two
-** @params:  x {unsigned int} - initial value
-** @returns: {unsigned int} - power of two closest to and greater than initial value
-*/
-MVLADEF unsigned int pot(unsigned int x) {
-  // from here (great read): https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
-  
-  // move all bits down while or'ing them in order to fill all
-  // lower bits with 1's, effectively making x 1 less than a
-  // power of 2
-  // ie.. 43 after x |= x >> 1 -> 0000000000111111 (63)
-  // this step continues, with no other bits changing, until a 1 is
-  // added at the end
-  
-  x--; // decrement x (flip lowest bit)
-  x |= x >> 1;
-  x |= x >> 2;
-  x |= x >> 4;
-  x |= x >> 8;
-  x |= x >> 16;
-  x++; // round up to power of 2
-  
-  return x;
-}
 
 /*
 ** @brief:   Generate a random float between 0 and 1
@@ -1895,200 +1848,6 @@ MVLADEF void vec_print(const Vec a) {
 */
 MVLADEF void vec_print_length(const Vec a) {
   printf("Vector Length: %d\n", a.length);
-}
-
-/*
-** @brief:   Create a fat pointer vector able to contain elements of size element_size
-** @params:  element_size {unsigned int} - size of each element in the vector
-** @returns: {void *} - pointer to buffer of vector
-*/
-MVLADEF void *fvec(unsigned int element_size) {
-  FVecData* v = calloc(1, sizeof(FVecData));
-  v->element_size = element_size;
-  v->capacity = 0;
-  v->length = 0;
-  v->bytes_alloc = v->capacity * element_size; // also 0
-
-  return &v->buffer;
-}
-
-/*
-** @brief:   Create a fat pointer vector able to contain elements of size element_size, with initial allocation of initial_size
-** @params:  element_size {unsigned int} - size of each element in the vector, initial_size {unsigned int} - number of initial allocated slots for elements in the vector
-** @returns: {void *} - pointer to buffer of vector
-*/
-MVLADEF void *fvecci(unsigned int element_size, unsigned int initial_size) {
-  initial_size = pot(initial_size);
-  
-  FVecData* v = calloc(1, sizeof(FVecData) + initial_size * element_size);
-  v->element_size = element_size;
-  v->capacity = initial_size; // create a capacity rounded up to a multiple of 2 from initial_size
-  v->length = 0;
-  v->bytes_alloc = v->capacity * element_size; // also 0
-  
-  return &v->buffer;
-}
-
-/*
-** @WARNING: !!! TAKE PRECAUTIONS THAT VECTOR POINTS TO VALID DATA !!!
-**
-** @brief:   Get the data behind a vector fat pointer
-** @params:  vector {void *} - fat pointer pointing to a buffer to underlying data
-** @returns: {FVecData *} - data behind the fat pointer vector
-*/
-MVLADEF FVecData *fvec_get_data(void *vector) {
-  assert(vector);
-  // total hack for pointer arithmetic
-  // cast vector to FVecData*, then index back by 1
-  // as the pointer being indexed is of type FVecData,
-  // it decrements back to the address of the start of the structure
-  return &((FVecData *)vector)[-1];
-}
-
-/*
-** @brief:   Clone a fat pointer vector
-** @params:  vector {void *} - fat pointer vector to clone
-** @returns: new_vector {void *} - new fat pointer vector pointing to separate memory
-*/
-MVLADEF void *fvec_clone(void *vector) {
-  assert(vector);
-  FVecData *v_data = fvec_get_data(vector);
-  void *new_vector = fvecci(v_data->element_size, v_data->length);
-  
-  memcpy(new_vector, vector, v_data->element_size * v_data->length);
-  
-  return new_vector;
-}
-
-/*
-** @brief:   Check if a vector has space for another element
-** @params:  v_data {FVecData *} - vector to check
-** @returns: {int} - a boolean value representing whether or not the vector has space
-*/
-MVLADEF int fvec_has_space(FVecData *v_data) {
-  //assert(v_data->bytes_alloc == (v_data->capacity * v_data->element_size));
-  return (v_data->bytes_alloc - (v_data->length * v_data->element_size)) > 0;
-}
-
-/*
-** @brief:   Expand a vector's allocation
-** @params:  v_data {FVecData **} - the vector to expand
-** @returns: N/A
-*/
-MVLADEF void fvec_expand(FVecData **v_data) {
-  assert((*v_data)->capacity >= (*v_data)->length);
-  assert((*v_data)->bytes_alloc == ((*v_data)->capacity * (*v_data)->element_size));
-
-  int old_bytes = sizeof(**v_data) + (*v_data)->bytes_alloc;
-  
-  if((*v_data)->bytes_alloc == 0) {
-    (*v_data)->capacity = 2;
-    (*v_data)->bytes_alloc = (*v_data)->capacity * (*v_data)->element_size;
-  } else {
-    (*v_data)->capacity *= 2;
-    (*v_data)->bytes_alloc = (*v_data)->capacity * (*v_data)->element_size;
-  }
-
-  //fprintf(stderr, "Reallocating from %d bytes to %d bytes!\n", old_bytes, sizeof(**v_data) + (*v_data)->bytes_alloc);
-
-  *v_data = realloc(*v_data, sizeof(FVecData) + (*v_data)->bytes_alloc);
-}
-
-/*
-** @brief:   Push a value into the vector
-** @params:  vector {void **} - fat pointer vector to push into
-** @returns: res {void *} - pointer to new address at the end of the vector
-*/
-MVLADEF void *fvec_push(void **vector) {
-  assert(vector);
-  FVecData *v_data = fvec_get_data(*vector);
-  
-  if(!fvec_has_space(v_data)) {
-    fvec_expand(&v_data);
-  }
-
-  v_data->length += 1;
-  if(!fvec_has_space(v_data)) {
-    fvec_expand(&v_data);
-  }
-
-  // make sure to increment the length
-  *vector = &v_data->buffer;
-  // return a void pointer to the next available slot in the vector (0 indexed, sub1)
-  void *res = *vector + v_data->element_size * (v_data->length-1);
-  
-  return res;
-}
-
-/*
-** @brief:   Remove the last element from a fat pointer vector, shrinking allocation if the length is a power of 2
-** @params:  vector {void **} - fat pointer vector to remove an element from
-** @returns: N/A
-*/
-MVLADEF void fvec_pop_back(void **vector) {
-  assert(vector);
-  FVecData *v_data = fvec_get_data(*vector);
-  assert(v_data->length > 0 && "Cannot pop an empty vector!");
-  
-  v_data->length -= 1;
-  
-  // if its a power of 2...
-  if(ceil(log2(v_data->length)) == floor(log2(v_data->length))) {
-    // know length is a power of two, capacity remains consistent
-    v_data->capacity = v_data->length;
-    v_data->bytes_alloc = v_data->element_size * v_data->length;
-    // shrink allocation
-    v_data = realloc(v_data, sizeof(FVecData) + v_data->bytes_alloc);
-  }
-
-  *vector = &v_data->buffer;
-}
-
-/*
-** @brief:   Maps a function of type :: Void -> Void* onto each element of a fat pointer vector
-** @params:  vector {void *} - fat pointer vector to map, func {void (*)(void*)} - function to apply to each vector element
-** @returns: N/A
-*/
-MVLADEF void fvec_map(void *vector, void (*func)(void*)) {
-  assert(vector);
-  FVecData *v_data = fvec_get_data(vector);
-  
-  for(int i = 0; i < v_data->length; ++i)
-    func(vector + i * v_data->element_size); // vector still valid -> no reallocations yet
-}
-
-/*
-** @brief:   Free a fat pointer vector (also sets pointer to NULL)
-** @params:  vector {void **} - reference to vector to free
-** @returns: N/A 
-*/
-MVLADEF void fvec_free(void **vector) {
-  FVecData *v_data = fvec_get_data(*vector);
-  free(v_data);
-  *vector = NULL;
-}
-
-/*
-** @brief:   Get the length of a fat pointer vector
-** @params:  vector {void *} - target for length
-** @returns: {unsigned int} - length of the vector
-*/
-MVLADEF unsigned int fvec_length(void *vector) {
-  FVecData *v_data = fvec_get_data(vector);
-  return v_data->length;
-}
-
-/*
-** @brief:   Print a fat pointer vector
-** @params:  vector {void *} - vector to print, print_func {void(*)(void*)} - function pointer to user function for printing elements (no newline expected)
-** @returns: N/A
-*/
-MVLADEF void fvec_print(void *vector, void(*print_func)(void*)) {
-  assert(vector);
-  FVecData *v_data = fvec_get_data(vector);
-  for(int i = 0; i < v_data->length; ++i)
-    print_func(vector + i * v_data->element_size); // vector still valid -> no reallocations yet
-  printf("\n");
 }
 
 // -----------------------------------------
