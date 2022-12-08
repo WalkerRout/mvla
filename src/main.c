ac18aa0a34461ca246f3b9e7bb318164234658d9
walkerrout04@gmail.com <walkerrout04@gmail.com>
2022-12-07 21:28:36 -0800

    Removed fvec tests

--- src/main.c
+++ src/main.c
@@ -2,18 +2,8 @@
 #include <time.h>
 
 #define MVLA_IMPLEMENTATION
-#include "mvla.h"
+#include "../mvla.h"
 #undef  MVLA_IMPLEMENTATION
-
-void map_int(void *i) {
-  int *ref = i;
-  *ref += 1;
-  *ref *= 2;
-}
-
-void print_int(void *i) {
-  printf("%d ", *(int*) i);
-}
 
 int main(void) {
   srand(time(NULL));
@@ -24,31 +14,10 @@ int main(void) {
   mat_fill_rand(&a);
   mat_fill_rand(&b);
 
-  printf("a: \n");
   mat_print(mat_mul(mat_add(a, b), a));
-
-  int *test = fvec(sizeof(int));
-  *(int*)fvec_push(&test) = 3;
-  *(int*)fvec_push(&test) = 4;
-  *(int*)fvec_push(&test) = 5;
-  fvec_pop_back(&test);
-  *(int*)fvec_push(&test) = 6;
-  *(int*)fvec_push(&test) = 7;
-  fvec_pop_back(&test);
-  *(int*)fvec_push(&test) = 8;
-  *(int*)fvec_push(&test) = 9;
-  fvec_pop_back(&test);
-  fvec_pop_back(&test);
-  fvec_pop_back(&test);
-  
-  fvec_map(test, map_int);
-  fvec_print(test, print_int);
-
-  printf("Vector length: %u\n", fvec_length(test));
 
   mat_free(&a);
   mat_free(&b);
-  fvec_free(&test);
   
   return 0;
 }
