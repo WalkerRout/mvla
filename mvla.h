86ccb04f3e9cc9593fd9eeeb119d66ab21423269
walkerrout04@gmail.com <walkerrout04@gmail.com>
2024-10-27 18:05:10 -0700

    trim includes

--- mvla.h
+++ mvla.h
@@ -33,12 +33,8 @@ extern "C" {
 ** INCLUDES
 */
 
-#include <assert.h>
 #include <math.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <time.h>
 
 // -----------------------------------------
 
